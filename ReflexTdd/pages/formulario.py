46746eec283d4042d2a78c243b7a516fae1fc807
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2025-02-13 11:49:37 +0100

    Creación formulario

---
+++ ReflexTdd/pages/formulario.py
@@ -0,0 +1,11 @@
+import reflex as rx
+
+import ReflexTdd.ReflexTdd
+
+@rx.page(route="/formulario")
+def formulario() -> rx.Component:
+    return rx.box(
+        rx.vstack(
+            
+        )
+    )
